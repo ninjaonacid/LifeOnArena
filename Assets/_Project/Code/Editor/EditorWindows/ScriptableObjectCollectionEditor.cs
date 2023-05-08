a67b2c6b781d0087bc78d4eb5135e5f8c1a83646
deadninja <ninjaonacid1@gmail.com>
2023-05-08 16:37:35 +0300

    editor window changes

--- Assets/_Project/Code/Editor/EditorWindows/ScriptableObjectCollectionEditor.cs
+++ Assets/_Project/Code/Editor/EditorWindows/ScriptableObjectCollectionEditor.cs
@@ -30,59 +30,134 @@ namespace Code.Editor.EditorWindows
             _createButton = this.Q<Button>("CreateButton");
             _toolbarSearchField = this.Q<ToolbarSearchField>();
             _propertyField = this.Q<PropertyField>();
-
-
         }
 
-        //public void Initialize(ScriptableObject target, List<T> items)
-        //{
-        //    _target = target;
-        //    _items = items;
-        //    InitializeInternal();
-        //}
+        public void Initialize(ScriptableObject target, List<T> items)
+        {
+            _target = target;
+            _items = items;
+            InitializeInternal();
+        }
 
-        //private void InitializeInternal()
-        //{
-        //    Func<VisualElement> makeItem = () => new Label();
-        //    _listView.makeItem = makeItem;
-        //    _listView.onSelectionChange += objects =>
-        //    {
-        //        T item = objects.First() as T;
-        //        Select(item);
-        //    };
+        private void InitializeInternal()
+        {
+            Func<VisualElement> makeItem = () => new Label();
+            _listView.makeItem = makeItem;
+            _listView.onSelectionChange += objects =>
+            {
+                T item = objects.First() as T;
+                Select(item);
+            };
 
-        //    Action<VisualElement, int> bindItem = (element, index) =>
-        //    {
-        //        Label label = element as Label;
-        //        label.AddManipulator(new ContextualMenuManipulator(evt =>
-        //        {
-        //            evt.menu.AppendAction("Duplicate", action =>
-        //            {
-        //                Duplicate(_filteredListView);
-        //            });
-        //            evt.menu.AppendAction("Remove", action =>
-        //            {
-        //                Remove(_filteredListView[index]);
-        //            });
+            Action<VisualElement, int> bindItem = (element, index) =>
+            {
+                Label label = element as Label;
+                label.AddManipulator(new ContextualMenuManipulator(evt =>
+                {
+                    evt.menu.AppendAction("Duplicate", action =>
+                    {
+                        Duplicate(_filteredListView[index]);
+                    });
+                    evt.menu.AppendAction("Remove", action =>
+                    {
+                        Remove(_filteredListView[index]);
+                    });
 
-        //        }));
+                }));
 
-        //        SerializedObject serializedObject = new SerializedObject(_filteredListView[index]);
-        //        SerializedProperty serializedProperty = serializedObject.FindProperty("_name");
-        //        label.BindProperty(serializedProperty);
-        //    };
+                SerializedObject serializedObject = new SerializedObject(_filteredListView[index]);
+                SerializedProperty serializedProperty = serializedObject.FindProperty("_name");
+                label.BindProperty(serializedProperty);
+            };
 
-        //    _listView.bindItem = bindItem;
-        //    _listView.itemsSource = _filteredListView = _items;
+            _listView.bindItem = bindItem;
+            _listView.itemsSource = _filteredListView = _items;
 
-        //    _createButton.clicked += Create;
+            _createButton.clicked += Create;
 
-        //    _toolbarSearchField.RegisterCallback<ChangeEvent<string>>(evt =>
-        //    {
-        //        _listView.itemsSource = _filteredListView = _items
-        //            .Where(item => item.name.StartsWith(evt.newValue, StringComparison.OrdinalIgnoreCase)).ToList();
-        //        _listView.Rebuild();
-        //    });
-        //}
+            _toolbarSearchField.RegisterCallback<ChangeEvent<string>>(evt =>
+            {
+                _listView.itemsSource = _filteredListView = _items
+                    .Where(item => item.name.StartsWith(evt.newValue, StringComparison.OrdinalIgnoreCase)).ToList();
+                _listView.Rebuild();
+            });
+        }
+
+        private void Duplicate(T item)
+        {
+            T duplicate = ScriptableObject.Instantiate(item);
+            duplicate.hideFlags = HideFlags.HideInHierarchy;
+            AssetDatabase.AddObjectToAsset(duplicate, _target);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            _items.Add(duplicate);
+            _listView.Rebuild();
+            Select(duplicate);
+            _listView.SetSelection(_items.Count - 1);
+            EditorUtility.SetDirty(_target);
+        }
+
+        private void Create()
+        {
+            Type[] types = AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(assembly => assembly.GetTypes()
+                    .Where(type => typeof(T).IsAssignableFrom(type) && type.IsClass && !type.IsAbstract))
+                    .ToArray();
+
+            if (types.Length > 1)
+            {
+                GenericMenu menu = new GenericMenu();
+
+                foreach (var type in types)
+                {
+                    menu.AddItem(new GUIContent(ObjectNames.NicifyVariableName(type.Name)), false, delegate
+                    {
+                        CreateItem(type);
+                    });
+                }
+
+                menu.ShowAsContext();
+            }
+            else
+            {
+                CreateItem(types[0]);
+            }
+        }
+
+        private void CreateItem(Type type)
+        {
+            T item = (T)ScriptableObject.CreateInstance(type);
+            item.name = "New item";
+            item.hideFlags = HideFlags.HideInHierarchy;
+            AssetDatabase.AddObjectToAsset(item, _target);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            _items.Add(item);
+            _listView.Rebuild();
+            Select(item);
+            _listView.SetSelection(_items.Count - 1);
+            EditorUtility.SetDirty(_target);
+        }
+
+        private void Select(T item)
+        {
+            SerializedObject serializedObject = new SerializedObject(item);
+            _inspector.Bind(serializedObject);
+            _propertyField.Bind(serializedObject);
+        }
+
+        private void Remove(T item)
+        {
+            if (EditorUtility.DisplayDialog("Delete item", "Are you sure you want to delete"
+                                                           + item.name + "?", "Yes", "No"))
+            {
+                ScriptableObject.DestroyImmediate(item, true);
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+                _items.Remove(item);
+                _listView.Rebuild();
+                EditorUtility.SetDirty(_target);
+            }
+        }
     }
 }
---
+++ Assets/_Project/Code/Editor/EditorWindows/StatDatabaseWindow/StatDatabaseEditorWindow.cs
@@ -0,0 +1,53 @@
+using Code.StaticData.StatSystem;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Code.Editor.EditorWindows.StatDatabaseWindow
+{
+    public class StatDatabaseEditorWindow : EditorWindow
+    {
+
+        private StatDatabase _dataBase;
+
+        [MenuItem("Window/StatSystem/StatDatabase")]
+        public static void ShowWindow()
+        {
+            StatDatabaseEditorWindow window = CreateInstance<StatDatabaseEditorWindow>();
+            window.Show();
+            window.minSize = new Vector2(800, 600);
+            window.titleContent = new GUIContent("StatDatabase");
+        }
+
+        [OnOpenAsset]
+        public static bool OnOpenAsset(int instanceId, int line)
+        {
+            if (EditorUtility.InstanceIDToObject(instanceId) is StatDatabase)
+            {
+                ShowWindow();
+                return true;
+            }
+
+            return false;
+        }
+
+        private void OnSelectionChanged()
+        {
+            _dataBase = Selection.activeObject as StatDatabase;
+        }
+
+        public void CreateGUI()
+        {
+            OnSelectionChanged();
+   
+            VisualElement root = rootVisualElement;
+ 
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/_Project/Code/Editor/EditorWindows/StatDatabaseWindow/StatDatabaseEditorWindow.uxml");
+            visualTree.CloneTree(root);
+
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/_Project/Code/Editor/EditorWindows/StatDatabaseWindow/StatDatabaseEditorWindow.uss");
+            root.styleSheets.Add(styleSheet);
+        }
+    }
+}
