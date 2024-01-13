578197ea0607ed3e2576848b35550c302dd6ae04
deadninja <ninjaonacid1@gmail.com>
2024-01-13 15:09:31 +0300

    no message

--- Assets/_Project/Code/Core/ObjectPool/ObjectPool.cs
+++ Assets/_Project/Code/Core/ObjectPool/ObjectPool.cs
@@ -1,7 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+using VContainer;
+
 namespace Code.Core.ObjectPool
 {
-    public class ObjectPool<T>
+    public class ObjectPool
     {
+        private IObjectResolver _resolver;
+        private GameObject _pooledObject;
+        private GameObject _poolRoot;
+
+        private Queue<GameObject> _objectsStock;
+        public ObjectPool(IObjectResolver resolver, GameObject pooledObject)
+        {
+            _resolver = resolver;
+            _pooledObject = pooledObject;
+        }
         
+        public void Initialize()
+        {
+            _poolRoot = new GameObject($"{_pooledObject.name} pool");
+            Object.DontDestroyOnLoad(_poolRoot);
+        }
+
+        public GameObject Spawn(Transform parent)
+        {
+           //if(_pooledObject.)
+           return null;
+        }
     }
 }
---
+++ Assets/_Project/Code/Core/ObjectPool/ObjectPoolProvider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using VContainer;
+
+namespace Code.Core.ObjectPool
+{
+    public class ObjectPoolProvider
+    {
+        private IObjectResolver _resolver;
+        private Dictionary<int, ObjectPool> _poolMap;
+
+        public ObjectPoolProvider(IObjectResolver resolver)
+        {
+            _resolver = resolver;
+        }
+        
+        public GameObject Spawn(GameObject go, Transform parent)
+        {
+            ObjectPool pool = null;
+            
+            if (!_poolMap.TryGetValue(go.GetInstanceID(), out pool))
+            {
+                pool = CreatePool(go);
+            }
+
+            GameObject spawnedObject = pool.Spawn(parent);
+            
+            return null;
+        }
+
+        public GameObject Despawn(GameObject go)
+        {
+            
+            return null;
+        }
+
+        private ObjectPool CreatePool(GameObject go)
+        {
+            var pool = new ObjectPool(_resolver, go);
+            return pool;
+        }
+    }
+}
