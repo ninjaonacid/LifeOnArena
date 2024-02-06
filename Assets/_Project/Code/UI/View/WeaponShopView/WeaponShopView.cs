b5d8691923214a267c56440f451bfede0d2cf7ef
deadninja <ninjaonacid1@gmail.com>
2024-02-06 17:17:04 +0300

    no message

---
+++ Assets/_Project/Code/UI/View/WeaponShopView/WeaponShopView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Code.UI.View.WeaponShopView
+{
+    public class WeaponShopView : MonoBehaviour, IScreenView
+    {
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Close()
+        {
+            Destroy(this);
+        }
+    }
+}
