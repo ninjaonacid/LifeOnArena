a32cad6b601dbde4d5661b93869b03bb1aa77830
deadninja <ninjaonacid1@gmail.com>
2024-05-26 16:31:53 +0300

    no message

--- Assets/_Project/Code/Runtime/UI/Controller/MainMenuController.cs
+++ Assets/_Project/Code/Runtime/UI/Controller/MainMenuController.cs
@@ -64,8 +64,8 @@ namespace Code.Runtime.UI.Controller
 
             _windowView.StartFightButton
                 .OnClickAsObservable()
-                .Subscribe(x => _screenService.Open(ScreenID.ArenaSelectionScreen)
-                    //_sceneLoader.Load("FantasyArena_1")
+                .Subscribe(x => //_screenService.Open(ScreenID.ArenaSelectionScreen)
+                    _sceneLoader.Load("FantasyArena_1")
                 );
 
             _windowView.SkillScreen.Button
