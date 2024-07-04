e9adcd2b9cd0e2f47a8456a140d705ba5177bf7e
deadninja <ninjaonacid1@gmail.com>
2024-07-05 00:54:11 +0300

    no message

---
+++ Assets/_Project/Code/Runtime/Logic/Environment/FlyingObelisk.cs
@@ -0,0 +1,19 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Code.Runtime.Logic.Environment
+{
+    public class FlyingObelisk : MonoBehaviour
+    {
+        [SerializeField] private Transform _topObelisk;
+        [SerializeField] private Transform _bottomObelisk;
+        private void Start()
+        {
+            _topObelisk.DOLocalMove(new Vector3(0, 4, 0), 2f).SetLoops(-1, LoopType.Yoyo).SetLink(gameObject);
+            _bottomObelisk.DOLocalMove(new Vector3(0, 2, 0), 2f).SetLoops(-1, LoopType.Yoyo).SetLink(gameObject);
+            _topObelisk.DOShakeRotation(3f, 5).SetLoops(-1, LoopType.Yoyo);
+            _bottomObelisk.DOShakeRotation(3f, 5).SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+}
