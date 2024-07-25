62ba71e6fe2fbd20f5d0a123ced7806f8b7ebf04
deadninja <ninjaonacid1@gmail.com>
2024-07-25 14:39:44 +0300

    no message

--- Assets/_Project/Code/Runtime/UI/ArrowUI.cs
+++ Assets/_Project/Code/Runtime/UI/ArrowUI.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 namespace Code.Runtime.UI
 {
-    public class ArrowUI : MonoBehaviour
+    public class ArrowUI : CanvasElement
     {
         private RectTransform _transform;
 
-        private void Awake()
+        protected override void Awake()
         {
+            base.Awake();
             _transform = GetComponent<RectTransform>();
         }
 
         public void Movement(Vector2 movementDirection)
         {
             _transform.DOAnchorPos(movementDirection, 1).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo).SetLink(this.gameObject);
-            
         }
     }
 }
---
+++ Assets/_Project/Code/Runtime/UI/Buttons/PortalButton.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Code.Runtime.UI.Buttons
+{
+    public class PortalButton : MonoBehaviour
+    {
+        public ArrowUI ArrowUI;
+    }
+}
